e2c3effdf46e750255dae5ec41e016f640cad0ca
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-06-15 15:10:49 -0300

    varias coisas que veio direto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <!-- <v-responsive class="align-center text-center fill-height">
+     <v-row align="center">
+     <v-col cols="2">1</v-col>
+     <v-col>2</v-col>
+     <v-col>3</v-col>
+     </v-row>
+     <v-row justify="center">
+     <v-col cols="2">1</v-col>
+     <v-col cols="5">2</v-col>
+     <v-col cols="4">3</v-col>  
+     </v-row>
+
+     <v-row align="end">
+     <v-col cols="2">1</v-col>
+     <v-col cols="5">2</v-col>
+     <v-col cols="12">3</v-col>  
+     </v-row>    </v-responsive> -->
+
+     <div class="custom d-flex flex-wrap"> 
+      <dir v-for="n in 10" :key="n" class="card">
+        <h3>Nome</h3>
+        <p>Descrição</p>
+      </dir>
+     </div>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style scoped>
+.custom {
+  width: 80%;
+  height: 800px;
+  background: plum;
+}
+
+.card {
+  width: 20%;
+  height: 10%;
+  background: palevioletred;
+  border: 1px solid pink;
+}
+.v-row {
+  background: red;
+  height: 200px;
+}
+.v-col {
+  background: blue;
+  border: 1px solid white;;
+}
+</style>
---
+++ src/views/Form.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data: () => ({
+    firstName: "",
+    rules: [
+      (value) => {
+        if (value) return true;
+
+        return "You must enter a first name.";
+      },
+    ],
+  }),
+  methods: {
+    handleSubmit (event) {
+        event.preventDefault();
+        if (!this.isFormValid)
+        alert("Preencha direito, por favor!")
+        return;
+    }
+  }
+};
+</script>
+
+<template>
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent>
+      <!-- input  -->
+      <v-text-field
+        v-model="firstName"
+        :rules="rules"
+        label="First name"
+      ></v-text-field>
+      <v-btn type="submit" :disabled="!isFormValid" @click="handleSubmit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
