427a48ad9e8b609e83f495f187ef577959f84dc5
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-06-16 14:22:34 -0300

    criação tela de login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,66 +1,70 @@
-<script setup>
-  //
-</script>
+<script>
+ import { RouterLink, RouterView} from 'vue-router'
 
+  export default {
+    data: () => ({
+      email: '',
+      emailRules: [
+        value => {
+          if (!value ) return 'Email must be at least 1 character.';
+          if (/\S+@\S+\.\S+/.test(value)) return true;
+          return 'Email must be valid';
+        },
+      ],
+      show1: false,
+      show2: true,
+      password: '',
+      passwordRules: [
+        value => {
+          if (!value) return 'Password must be at least 1 character.'
+        },
+      ],
+      isFormValid: true
+    }),
+    methods: {
+    handleSubmit (event) {
+        event.preventDefault();
+        if (!this.isFormValid)
+        return;
+    }
+  }
+  }
+</script>
 
 <template>
+  <v-sheet width="300" class="mx-auto">
+    
+    <v-form @submit.prevent v-model="isFormValid"> 
+      <v-text-field
+        v-model="email"
+        type="emai"
+        label="Email"
+        clearable
+        hide-details="auto"
+        :rules="emailRules">
+      </v-text-field>
 
-<h3>ALO</h3>
+
+      <v-text-field
+        v-model="password"
+        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show1 ? 'text' : 'password'"
+        label="Password"
+        @click:append="show1 = !show1"
+        clearable
+        hide-details="auto"
+        :rules="passwordRules"
+      ></v-text-field>
+
+      <v-btn type="submit" :disabled="!isFormValid" @click="handleSubmit" block class="mt-2">Login</v-btn>
+    </v-form>
+  </v-sheet>
+  <RouterLink id="btn" type="button" to="cadastro">Cadastro</RouterLink>
+  <RouterView />
 </template>
 
-
-<!--<template>
-  <v-container>
-    <v-responsive class="align-center text-center fill-height">
-     <v-row align="center">
-     <v-col cols="2">1</v-col>
-     <v-col>2</v-col>
-     <v-col>3</v-col>
-     </v-row>
-     <v-row justify="center">
-     <v-col cols="2">1</v-col>
-     <v-col cols="5">2</v-col>
-     <v-col cols="4">3</v-col>  
-     </v-row>
-
-     <v-row align="end">
-     <v-col cols="2">1</v-col>
-     <v-col cols="5">2</v-col>
-     <v-col cols="12">3</v-col>  
-     </v-row>    </v-responsive>
-
-     <div class="custom d-flex flex-wrap"> 
-      <dir v-for="n in 10" :key="n" class="card">
-        <h3>Nome</h3>
-        <p>Descrição</p>
-      </dir>
-     </div>
-  </v-container>
-</template>
-
-<script setup>
-  //
-</script>
-
-<style scoped>
-.custom {
-  width: 80%;
-  height: 800px;
-  background: plum;
+<style> 
+#btn {
+  border: 1px solid red;
 }
-
-.card {
-  width: 20%;
-  height: 10%;
-  background: palevioletred;
-  border: 1px solid pink;
-}
-.v-row {
-  background: red;
-  height: 200px;
-}
-.v-col {
-  background: blue;
-  border: 1px solid white;;
-}
-</style>  -->
+</style>
