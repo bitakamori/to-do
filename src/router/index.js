c8b14b682d68900c1aa230e4250cb7b2bf34d3c5
Karolina <karolinaborgesk@gmail.com>
2023-06-27 09:20:28 -0300

    ajustes de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,62 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
+        path: "",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
       },
       {
-        path: 'form',
-        name: 'Form',
-        component: () => import ('@/views/Form.vue'),
+        path: "form",
+        name: "Form",
+        component: () => import("@/views/Form.vue"),
       },
       {
-        path: 'cadastro',
-        name: 'Cadastro',
-        component: () => import ('@/views/Cadastro.vue'),
+        path: "cadastro",
+        name: "Cadastro",
+        component: () => import("@/views/Cadastro.vue"),
       },
       {
-        path: 'login',
-        name: 'Login',
-        component: () => import ('@/views/Login.vue'),
+        path: "login",
+        name: "Login",
+        component: () => import("@/views/Login.vue"),
       },
     ],
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import ('@/views/Dashboard.vue'),
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("@/views/Dashboard.vue"),
   },
-]
+  {
+    path: "/new-list",
+    name: "CreateList",
+    component: () => import("@/views/CreateList.vue"),
+  },
+  {
+    path: "/list-detail/:id",
+    name: "ListDetail",
+    component: () => import("@/views/DetailList.vue"),
+  },
+  {
+    path: "/teste",
+    name: "teste",
+    component: () => import("@/views/teste.vue"),
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
